10f7c1e35d4f357ee53ed14d00b56cd3edd04516
ali <nextup@gmail.com>
2018-05-22 00:42:25 +0430

    Icon theme changed!

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react';
+
+
+
+class LoginForm extends React.Component {
+    state = {
+        data: {
+            email: '',
+            password: '',
+        },
+        loading: false,
+        errors: {}
+    };
+
+    onChange = e => 
+    this.setState({ data: { ...this.state.data, [e.target.name]: e.target.value }})
+
+    render() {
+        const { data } = this.state;
+        return (
+            <Form>
+                <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="example@example.com"
+                        value= {data.email}
+                        onChange={this.onChange} />
+                </Form.Field>
+                <Button primary>Login</Button>
+            </Form>
+        );
+    }
+}
+
+export default LoginForm;
